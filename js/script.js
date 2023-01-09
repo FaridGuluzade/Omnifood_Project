b7ee6ed5eb22e7edfb03cb21f5f72b736d7378ca
Farid <faridguluzade26@gmail.com>
2023-01-08 17:13:17 -0800

    functionality uptated

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const nav = document.querySelector(".main-nav");
 const sectionHero = document.querySelector(".section-hero");
 const body = document.querySelector("body");
 const yearEl = document.querySelector(".year");
+const navList = document.querySelector(".main-nav-list");
 
 // Make mobile navigation work
 btnMobile.addEventListener("click", function () {
@@ -15,6 +16,34 @@ btnMobile.addEventListener("click", function () {
 // Set current year
 const currentYear = new Date().getFullYear();
 yearEl.textContent = currentYear;
+
+// Smooth scrolling animation
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      header.classList.toggle("nav-open");
+  });
+});
 
 // Sticky navigation
 const stickyNav = function (entries) {
